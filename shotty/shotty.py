cc731836849a130a7aceb54a67cf6e486e9c1a5f
MA Hassan Sheikh <ma.hassan.sheikh@metrotrains.com.au>
2020-07-06 11:29:18 +1000

    updating script to list availability zone, type , state and dnz of ec2 instances. added click package too

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,9 +1,19 @@
 import boto3
+import click
+session = boto3.Session(profile_name='nouh')
+ec2 = session.resource('ec2')
+@click.command()
 
-if __name__ == '__main__':
-
-    session = boto3.Session(profile_name='nouh')
-    ec2 = session.resource('ec2')
-
+def list_instances():
+    "List ec2 instances"
     for i in ec2.instances.all():
-        print(i)
+        print(','.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+    return
+            
+if __name__ == '__main__':
+    list_instances()
